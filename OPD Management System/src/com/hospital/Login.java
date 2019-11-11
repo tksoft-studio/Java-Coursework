ff3f28b00c434432556a0aef9483325715d45684
kavidupraba <47698768+kavidupraba@users.noreply.github.com>
2019-11-11 09:22:34 +0530

    Change the dispose command to exit

--- OPD Management System/src/com/hospital/Login.java
+++ OPD Management System/src/com/hospital/Login.java
@@ -134,7 +134,7 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         //Exit the programme
-        this.dispose();
+        System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
